20dcd83c09b65fc0973a9853cbe749e3e4483c13
Roman Martinez <roman.mx3@gmail.com>
2018-05-08 20:54:53 +1000

    Added sample page template

---
+++ tpl-full-width.php
@@ -0,0 +1,22 @@
+<?php // Template Name: Full Width ?>
+<?php get_header() ?>
+<?php the_post() ?>
+
+
+
+<main class="main-row">
+	<div class="wrapper">
+		
+		
+		<article id="content" <?php post_class() ?>>
+			<h1><?php the_title() ?></h1>
+			<?php the_content() ?>
+		</article><!-- #content -->
+		
+		
+	</div><!-- .wrapper -->
+</main><!-- .main-row -->
+
+
+
+<?php get_footer() ?>
