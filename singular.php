d0d2e4c2a4b97da9fa0a41581cf4ced919c4b537
Roman Martinez <roman.mx3@gmail.com>
2018-05-08 20:32:32 +1000

    Added singular.php

---
+++ singular.php
@@ -0,0 +1,36 @@
+<?php get_header() ?>
+<?php the_post() ?>
+
+
+
+<main class="main-row">
+	<div class="wrapper">
+		
+		
+		<div class="page-container">
+			<div class="page-content">
+				
+				
+				<article id="content" <?php post_class() ?>>
+					<h1><?php the_title() ?></h1>
+					<?php the_content() ?>
+				</article><!-- #content -->
+				
+				
+			</div><!-- .page-content -->
+			<div class="page-sidebar">
+				
+				
+				<?php get_sidebar() ?>
+				
+				
+			</div><!-- .page-sidebar -->
+		</div><!-- .page-container -->
+		
+		
+	</div><!-- .wrapper -->
+</main><!-- .main-row -->
+
+
+
+<?php get_footer() ?>
