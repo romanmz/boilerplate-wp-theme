0020845db8cbbb90fdfe70f22dcb28855925f1f1
Roman Martinez <roman.mx3@gmail.com>
2019-02-12 23:31:19 -0600

    Added THEME_INC constant

--- functions.php
+++ functions.php
@@ -11,11 +11,12 @@ define( 'THEME_FAVICONS', THEME_URL.'/assets/favicons' );
 define( 'THEME_IMAGES', THEME_URL.'/assets/images' );
 define( 'THEME_SCRIPTS', THEME_URL.'/assets/js' );
 define( 'THEME_DIR', get_template_directory() );
+define( 'THEME_INC', THEME_DIR.'/inc' );
 
 // Include files
-require_once THEME_DIR.'/inc/theme-functions.php';
-require_once THEME_DIR.'/inc/theme-setup.php';
-require_once THEME_DIR.'/inc/theme-filters.php';
-require_once THEME_DIR.'/inc/shortcodes/icon.php';
-require_once THEME_DIR.'/inc/shortcodes/multi-column.php';
-require_once THEME_DIR.'/inc/shortcodes/sample-html.php';
+require_once THEME_INC.'/theme-functions.php';
+require_once THEME_INC.'/theme-setup.php';
+require_once THEME_INC.'/theme-filters.php';
+require_once THEME_INC.'/shortcodes/icon.php';
+require_once THEME_INC.'/shortcodes/multi-column.php';
+require_once THEME_INC.'/shortcodes/sample-html.php';
